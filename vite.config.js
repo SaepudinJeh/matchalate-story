61737877e5aab6fc94d91420dff8e7a1c485d3f1
SaepudinJeh <saepudinjeh@gmail.com>
2023-03-01 03:48:22 +0700

    test micro FE

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,39 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import federation from '@originjs/vite-plugin-federation'
 
+import pkg from './package.json';
+
+
+
+const federationPluginOpt = {
+  name: 'myself_story',
+  filename: 'remoteEntry.js',
+  exposes: {
+    './App': 'src/App',
+  },
+  shared: {
+    ...Object.entries(pkg.dependencies).reduce((acc, [key, version]) => {
+      acc[key] = {
+        version: version,
+      };
+      return acc;
+    }, {}),
+  },
+  // react: { singleton: true }, "react-dom": { singleton: true } },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    federation({
-      name: 'myself_story',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App',
-        './assets/images': './src/assets/*'
-      },
-      shared: ['react', 'react-dom']
-    })
+    federation(federationPluginOpt)
   ],
   build: {
-    modulePreload: false,
-    target: 'esnext',
     minify: false,
-    cssCodeSplit: false
+    sourcemap: true,
+    target: 'esnext',
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   }
 })
